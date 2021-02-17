0765dc484f883ce2d5cab4c37b3a61c2abbea1a7
Swati Shelke <ghogareswati14@gmail.com>
2021-02-17 10:09:46 +0530

    make it better

---
+++ backend/data/products.js
@@ -0,0 +1,107 @@
+const products = [
+  {
+    name: "Bule shirt",
+    image: "/images/shirt1.jpg",
+    description: `Fit Type: Regular Fit
+      Material - 75% Cotton and 25% Polyester
+      Regular fit and Full sleeve shirt
+      Collar style - Semi cut away collar; Cuff style - Single cuff
+      Pattern - Solid Men’s Formal Shirt
+      Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm iron
+      Made in India`,
+    brand: " LEVIZO",
+    category: "Fashion",
+    price: 889.99,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 3,
+  },
+  {
+    name: "Casual Plain Cotton Shirt",
+    image: "/images/shirt2.jpg",
+    description: `Fit Type: Regular Fit
+    Material - 75% Cotton and 25% Polyester
+    Regular fit and Full sleeve shirt
+    Collar style - Semi cut away collar; Cuff style - Single cuff
+    Pattern - Solid Men’s Formal Shirt
+    Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm iron
+    Made in India`,
+    brand: "Raymond",
+    category: "Fashion",
+    price: 599.99,
+    countInStock: 10,
+    rating: 4.3,
+    numReviews: 2,
+  },
+  {
+    name: "Casual Shirts for Men",
+    image: "/images/shirt3.jpg",
+    description: `Fit Type: Regular Fit
+    Material - 75% Cotton and 25% Polyester
+    Regular fit and Full sleeve shirt
+    Collar style - Semi cut away collar; Cuff style - Single cuff
+    Pattern - Solid Men’s Formal Shirt
+    Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm iron
+    Made in India`,
+    brand: "Allen Solly",
+    category: "Fashion",
+    price: 929.99,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 0,
+  },
+  {
+    name: "COODRONY Men Shirt Mens Business",
+    image: "/images/shirt4.jpg",
+    description: `Fit Type: Regular Fit
+    Material - 75% Cotton and 25% Polyester
+    Regular fit and Full sleeve shirt
+    Collar style - Semi cut away collar; Cuff style - Single cuff
+    Pattern - Solid Men’s Formal Shirt
+    Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm iron
+    Made in India`,
+    brand: "Allen Solly",
+    category: "Fashion",
+    price: 999.99,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 4,
+  },
+  {
+    name:
+      "High Quality Men Dress Shirt Long Sleeve Camisa Social Slim Fit Flower Shirt Men Fas",
+    image: "/images/shirt5.jpg",
+    description: `Fit Type: Regular Fit
+    Material - 75% Cotton and 25% Polyester
+    Regular fit and Full sleeve shirt
+    Collar style - Semi cut away collar; Cuff style - Single cuff
+    Pattern - Solid Men’s Formal Shirt
+    Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm iron
+    Made in India`,
+    brand: "Raymond",
+    category: "Fashion",
+    price: 949.99,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    name: "Men Navy & Green Slim Fit Checked Casual Shirt",
+    image: "/images/shirt6.jpg",
+    description: `Fit Type: Regular Fit
+    Material - 75% Cotton and 25% Polyester
+    Regular fit and Full sleeve shirt
+    Collar style - Semi cut away collar; Cuff style - Single cuff
+    Pattern - Solid Men’s Formal Shirt
+    Machine wash warm, wash dark colors separately, do not bleach, tumble dry low, warm iron
+    Made in India`,
+    brand: " LEVIZO",
+    category: "Fashion",
+    price: 599.99,
+    countInStock: 0,
+    rating: 4.2,
+    numReviews: 6,
+  },
+];
+
+export default products;
