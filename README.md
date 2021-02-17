fa4ae1de39e3462f4facde162c60be3d2621e38a
vinit-poojary-au8 <vinitpoojary00@gmail.com>
2021-02-17 13:25:30 +0530

    deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+https://swati-ecommerce.herokuapp.com/
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Meta from "../components/Meta";
+import { listProducts } from "../actions/productActions";
+
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
+
+  return (
+    <>
+      <Meta />
+      <h1>Latest Products</h1>
+      <>
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </>
+    </>
+  );
+};
+
+export default HomeScreen;
