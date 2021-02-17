43ded2d8c8faa374dda5d5fa189eaf586234dfdb
vinit-poojary-au8 <vinitpoojary00@gmail.com>
2021-02-17 13:53:46 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,54 @@
-const express =require("express");
-const dotenv =require( "dotenv");
-const colors =require( "colors");
-const morgan =require( "morgan");
-const { notFound, errorHandler } =require( "./middleware/errorMiddleware.js");
-const connectDB =require( "./config/db.js");
-const path =require( "path");
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
+const connectDB = require('./config/db.js')
+const path = require('path')
 
-const productRoutes =require( "./routes/productRoutes.js");
-const userRoutes =require( "./routes/userRoutes.js");
-const orderRoutes =require( "./routes/orderRoutes.js");
-const uploadRoutes =require( "./routes/uploadRoutes.js");
+const productRoutes = require('./routes/productRoutes.js')
+const userRoutes = require('./routes/userRoutes.js')
+const orderRoutes = require('./routes/orderRoutes.js')
+const uploadRoutes = require('./routes/uploadRoutes.js')
 
-dotenv.config();
+dotenv.config()
 
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
 }
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+const __dir = path.resolve()
+app.use('/uploads', express.static(path.join(__dir, '/uploads')))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dir, '/frontend/build')))
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dir, 'frontend', 'build', 'index.html'))
+	)
 } else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
+	app.get('/', (req, res) => {
+		res.send('API is running....')
+	})
 }
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
